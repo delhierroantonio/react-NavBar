583f2b752cd211759e4acb00a7408d7e773c2397
antoniodelhierro <delhierroantonio@icloud.com>
2022-07-18 23:42:32 -0500

    cambiar el nombre del componente cartListItem por ItemListContainer y pasar un mensaje por props al mismo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemListContainer />
+      <ItemListContainer greetings={'Hola, aquí ira mi catalogo de productos!'}/>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,14 +2,14 @@ import React from 'react'
 import cartLogo from '../../logos/cartLogo.png'
 import ItemCount from '../ItemCount/ItemCount'
 
-const ItemListContainer = () => {
+const ItemListContainer = (props) => {
   const onAdd = (cantidad) => {
     console.log(`la cantidad a comprar sera: ${cantidad}`)
   }
   return (
     <div>
       <img src={cartLogo} alt='cart button' />
-      <p>Aqui ira mi catalogo de productos</p>
+      <h1>{props.greetings}</h1>
       <ItemCount initial={1} stock={5} onAdd={onAdd} />
     </div>
   )
